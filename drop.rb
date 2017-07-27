07e20fc0c2d3e66f9e6240e2727b25a602910c0e
Bryan Gilbert <gilbertw1@gmail.com>
2017-07-26 20:58:34 -0400

    update for 0.2.0 release

--- drop.rb
+++ drop.rb
@@ -1,8 +1,8 @@
 class Drop < Formula
   desc "A simple screenshot, screencast, and file upload tool with S3 support"
   homepage "https://github.com/gilbertw1/drop"
-  url "https://github.com/gilbertw1/drop/archive/0.1.4.1.tar.gz"
-  sha256 "90c1d3fa009717ae85985706c95f314917afa7aaf3a370d4cccd23e877ed2c20"
+  url "https://github.com/gilbertw1/drop/archive/0.2.0.tar.gz"
+  sha256 "38ffd732c4eb071e5b6e5a154605fa2f49150861fdf2d12673edc858580b2943"
   head "https://github.com/gilbertw1/drop.git"
 
   depends_on "rust" => :build
@@ -13,8 +13,6 @@ class Drop < Formula
     bin.install "target/release/drop"
     man1.install "doc/drop.1"
 
-    # Completion scripts are generated in the crate's build directory, which
-    # includes a fingerprint hash. Try to locate it first
     out_dir = Dir["target/release/build/drop-*/out"].first
     bash_completion.install "#{out_dir}/drop.bash-completion"
     fish_completion.install "#{out_dir}/drop.fish"
